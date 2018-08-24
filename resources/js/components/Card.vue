01a1d325a47f2b23a5085e34dc86aa9230145193
Marian Gabriel <vladmariang@gmail.com>
2018-08-25 00:46:41 +0300

    Add saving and fix modal buttons

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -14,13 +14,16 @@
 			        <div class="bg-white rounded-lg shadow-lg overflow-hidden" style="width: 800px;">
 			            <div class="p-8">
 			                <heading :level="2" class="mb-6">Edit Environment File</heading>
-			                <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" cols="30" rows="20" v-model="content"></textarea>
+			                <textarea class="w-full h-full form-control form-input form-input-bordered py-3 min-h-textarea" cols="30" rows="20" v-model="value"></textarea>
 			            </div>
 
 			            <div class="bg-30 px-6 py-3 flex">
 			                <div class="ml-auto">
 			                    <button type="button" data-testid="cancel-button" @click.prevent="handleCancel" class="btn text-80 font-normal h-9 px-3 mr-3 btn-link">Cancel</button>
-			                    <button ref="confirmButton" data-testid="confirm-button" @click.prevent="handleSave" class="btn btn-default btn-danger">Save</button>
+			                    <button ref="confirmButton" data-testid="confirm-button" :disabled="isSaving" @click.prevent="handleSave" class="btn btn-default btn-danger" :class="{ 'cursor-not-allowed': isSaving, 'opacity-50': isSaving }">
+			                    	<span v-if="isSaving">Saving</span>
+			                    	<span v-else>Save</span>
+			                    </button>
 			                </div>
 			            </div>
 			        </div>
@@ -36,20 +39,21 @@ export default {
 
     data() {
         return {
-        	content: '',
-            isModalOpen: false
+        	value: '',
+            isModalOpen: false,
+            isSaving: false,
         }
-    },
-
-    mounted() {
-        //
     },
 
     methods: {
     	handleButton() {
     		this.isModalOpen = true
 
-    		this.content = ' asasa'
+    		Nova.request()
+	        	.get(`/nova-vendor/nova-env-card/environment`)
+	        	.then(response => {
+	        		this.value = response.data;
+	        	})
     	},
 
         handleCancel() {
@@ -57,10 +61,18 @@ export default {
         },
 
         handleSave() {
-            // set button with loading state
-            // send modifier .env to controller
-            // hide modal
+        	this.isSaving = true
+
+        	Nova.request()
+	        	.post(`/nova-vendor/nova-env-card/environment`, {
+	            	value: this.value
+	        	})
+	        	.then(response => {
+	        		this.isSaving = false
+	        		this.isModalOpen = false
+	        	})
         },
     },
 }
 </script>
+
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Card API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you may register API routes for your card. These routes
-| are loaded by the ServiceProvider of your card. You're free to add
-| as many additional routes to this file as your card may require.
-|
-*/
+Route::get('/environment', function (Request $request) {
+    return file_get_contents(base_path('.env'));
+});
 
-// Route::get('/endpoint', function (Request $request) {
-//     //
-// });
+Route::post('/environment', function (Request $request) {
+    file_put_contents(base_path('.env'), $request->value);
+});
